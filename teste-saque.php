7093ba6e0eb324cdd4f7baf4be2f6934cbedef1d
Lucas Souza <lucas_nevestj@hotmail.com>
2021-10-10 17:11:31 -0300

    comentários informativos nos arquivos

--- teste-saque.php
+++ teste-saque.php
@@ -5,6 +5,7 @@ use Alura\Banco\Modelo\{Cpf, Endereco};
 
 require_once 'autoload.php';
 
+//como a classe pai, Conta, é abstrata, sempre vamos inciar uma nova conta por meio de uma das classes filhas (ContaCorrente, ContaPoupanca, etc...)
 $novaConta = new ContaCorrente(
     new Titular(
         new Cpf('623.478.208-10'), 'Ana Clara', new Endereco('Gotham', 'Narrows', 'Docas', '1035')
